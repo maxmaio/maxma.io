1614096a92ab1e0057f218586b1b38fd846dee55
Max Maio <maxmaio8@gmail.com>
2020-02-29 21:53:14 -0600

    gravatar working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
 
 render(){
   return (
-    <Container>
-        <Grid container justify='center' align='center' spacing={3}>
-           <p>
-             hello
-           </p>
-        </Grid>
-    </Container>
+    <div className="header">
+      <img className="gravatar" src="https://www.gravatar.com/avatar/4934318e81ec19904134280eb795b7ad?s=600" alt="avatar" />
+      <h4>Max Maio</h4>
+  </div>
   );
 }
 }
