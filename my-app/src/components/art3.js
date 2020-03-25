c6cb0eb0cfee8a3c35fe5f133ee311147f403ec5
Max Maio <maxmaio8@gmail.com>
2020-03-24 23:43:43 -0400

    small art3 fix

--- my-app/src/components/art3.js
+++ my-app/src/components/art3.js
@@ -382,7 +382,7 @@ render() {
     return(
   <Container>
       <Container className="art333">
-          <p>click on canvas below to restart flow</p>
+          <p>aesthetic visualization</p>
          <p>to go back to homepage click <a href="/">here</a> </p>
       </Container>
   </Container>             
