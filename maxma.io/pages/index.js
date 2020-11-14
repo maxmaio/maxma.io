b3345786541cf58d4671cfd68f70e6d839acaf35
maxmaio <maxmaio8@gmail.com>
2020-11-14 10:36:31 -0800

    alligned bio accross site

--- maxma.io/pages/index.js
+++ maxma.io/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../styles/Home.module.css'
-import {Nav, Navbar, NavItem, NavLink} from 'reactstrap';
+import {Nav,  NavItem, NavLink} from 'reactstrap';
 import dynamic from 'next/dynamic'
 
 
@@ -46,16 +46,7 @@ export default function Home() {
       </div>
 
         </div>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+
 
 
       <DynamicComponent />
