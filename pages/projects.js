04af7dd6b52ebbb91eb81f387ea52ff8000126c3
maxmaio <maxmaio8@gmail.com>
2020-11-14 22:32:57 -0800

    increased size of projects

--- pages/projects.js
+++ pages/projects.js
@@ -14,7 +14,7 @@ export default function projects() {
                         width={100}
                         height={100}
                     />
-                    <h5>Max Maio</h5>
+                    <h4>Max Maio</h4>
                     <p>Software Engineer in CA</p>
 
                     <div >
@@ -34,11 +34,11 @@ export default function projects() {
 
             </div>
             <div className={styles.main}>
-                <h6>Web Visualizations</h6>
+                <h5>Web Visualizations</h5>
                 <p><a href="/art1">Viz1</a> - Visualization that draws lines from a single point outwards. No lines can collide and their direction is randomly generated.</p>
                 <p><a href="/art2">Viz2</a> - A variation of Viz1, except the lines are drawn following a cosine and sine formula.</p>
                 <p><a href="linesdemo">Lines Graphic</a> - Interactive graphics visualization featured on the <a href="/">home page</a>. Built with <a href="https://threejs.org/" target="_blank">Three.js</a>.</p>
-                <h6>Cool Projects</h6>
+                <h5>Cool Projects</h5>
                 <p><a href="https://github.com/maxmaio/mips-compiler" target="_blank">Mips-Compiler</a> - A compiler that compiles a subset of the C language to MIPS assembly code. Written in Java.</p>
                 <p><a href="https://github.com/maxmaio/Fantasy-football-stat-api" target="_blank">Fantasy Football Stat API</a> - Python program that retrieves league specific Yahoo fantasy football data and outputs into csv.</p>
                 <p><a href="https://www.notion.so/maxmaio/Surfline-Hack-afbfdb60aeaa4373b2aa85dd2b5a8492" target="_blank">Surf Hack</a> - Script that edits surfline's code and removes ads from live cams.</p>
